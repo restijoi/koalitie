1a7e1e901211c27bb3a66baa57dba9db969f27f2
Restie Antiquin <restiboi@Restys-MacBook-Pro.local>
2019-04-01 10:19:41 +0800

    setting up boiler plate for the site

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var statement = new Swiper('#statement', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
